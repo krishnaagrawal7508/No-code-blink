c811653bd963f63c5413f0c059d4884374cee9fc
krishna agrawal <agrawalkrishna522@gmail.com>
2024-07-10 20:34:19 +0530

    added get for blink

--- backend/index.js
+++ backend/index.js
@@ -28,14 +28,40 @@ if (!fs.existsSync('uploads')) {
 }
 
 // Handle file upload
+let id = 1;
 app.post('/upload', upload.single('coverImage'), (req, res) => {
   const { title, description } = req.body;
+  console.log(req.body, req.file.filename);
+
+  ++id;
+
   res.send({
     message: 'File uploaded successfully',
-    file: req.file,
+    id: id,
+    file: req.file.filename,
     title: title,
     description: description,
   });
+});
+
+app.get('/router_get/:id', (req, res) => {
+  let obj = {}
+  let id = req.params.id;
+
+  obj.icon = "/upload/" + req.params.image;
+  obj.title = req.params.title;
+  obj.description = req.params.description;
+  
+  console.log("here!!")
+  obj.links = {
+      "actions": [
+          {
+              "label": "Send",
+              "href": "http://localhost:8000/router_post/" + id ,
+          }
+      ]
+  }
+  res.send(JSON.stringify(obj));
 });
 
 app.listen(port, () => {
